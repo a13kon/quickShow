1416de2896f2e1bac5b31731e25d7d3fa1a86d5f
a13con <a13kon@yandex.ru>
2024-02-19 12:13:24 +0300

    first commit

---
+++ Exceltest/BO/ExcelHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Exceltest.BO
+{
+    class ExcelHelper: IDisposable
+    {
+        private string _filePath;
+        private Excel.Worksheet _worksheet;
+        private Excel.Application _excel;
+        private Excel.Workbook _workbook;
+        private Excel.Range _range;
+
+        //Excel.Application _excel = new Excel.Application();
+        //Excel.Workbook xlWorkbook = xlApp.Workbooks();
+        //Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+        //Excel.Range xlRange = xlWorksheet.UsedRange;
+
+        public ExcelHelper()
+        {
+            _excel = new Excel.Application();
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _workbook.Close(null, null, null);
+     
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        internal object Get(string column, int row)
+        {
+            try
+            {
+                return _range.Cells[row, column].Value2.ToString();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                return "empty";
+        }
+
+        internal bool Set(string column, int row, object data)
+        {
+            try
+            {
+                ((Excel.Worksheet)_excel.ActiveSheet).Cells[row, column] = data;
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+        internal void Save()
+        {
+            if (!string.IsNullOrEmpty(_filePath))
+            {
+                _workbook.SaveAs(_filePath);
+                _filePath = null;
+            }
+            else
+            {
+                _workbook.Save();
+            }
+        }
+
+        internal bool Open(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    _workbook = _excel.Workbooks.Open(filePath);
+                    _worksheet = _workbook.Sheets[1];
+                    _range = _worksheet.UsedRange;
+
+            }
+                else
+                {
+                    //_workbook = _excel.Workbooks.Add();
+                    //_filePath = filePath;
+                    Console.WriteLine("File not found");
+                    return false;
+            }
+            return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+    }
+}
---
+++ Exceltest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using EasyModbus;
+using Exceltest.BO;
+
+namespace Exceltest
+{
+    class Program
+    {
+        private static object result;
+
+
+        static void Main(string[] args)
+        {
+            BO.ExcelHelper helper = new BO.ExcelHelper();
+
+            ModbusClient modbusClient = new ModbusClient("COM10");
+            modbusClient.Baudrate = 19200;
+            modbusClient.UnitIdentifier = 1;
+            modbusClient.UDPFlag = true;
+                try
+                {
+                 
+
+                    
+                    {
+                        if (helper.Open(@"\\rubus\Сектор Электроники и автоматики\= Шмаглиенко\test.xlsx"))
+                        {
+                            
+                            helper.Set(column: "A", row: 1, data: "='[Лист Microsoft Excel — копия.xlsx]Лист1'!$A$1");
+                            helper.Save();
+
+                            result = helper.Get(column: "A", row: 1);
+                            
+                            modbusClient.Connect();
+                            modbusClient.WriteSingleRegister(0, Convert.ToInt32(result));
+                            
+                            Console.WriteLine(result);
+                        }
+                    }
+                
+                }
+                catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+            helper.Dispose();
+            modbusClient.Disconnect();
+            //Console.Read();
+            }
+        }
+    }
+
